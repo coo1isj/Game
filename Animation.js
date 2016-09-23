0414be72c25fb6251f9963dd5bbe378b6635076d
coo1isj <coo1piperni@gmail.com>
2016-09-23 15:40:47 -0400

    Worked on Attack animation

--- Animation.js
+++ Animation.js
@@ -1,3 +1,27 @@
-$(document).ready(function() {
-    $('#attackbox').fadeOut(1000);
+$(document).ready(function () {
+
+    var startTop = $('#attack').css("top");
+    var isMoving = false;
+
+    $('#attackbox').mouseenter(function () {
+        if (!isMoving) {
+            isMoving = true;
+            $('#attack').animate({
+                top: '100px'
+            }, 1000, function () {
+                isMoving = false;
+            });
+        }
+
+    });
+    $('#attackbox').mouseleave(function () {
+        if (!isMoving) {
+            isMoving = true;
+            $('#attack').animate({
+                top: startTop
+            }, 1000, function() {
+                isMoving = false;
+            });
+        }
+    });
 });
