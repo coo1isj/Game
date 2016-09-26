8763ed6031e0340893e3beb78f52f93b0613d06b
coo1isj <coo1piperni@gmail.com>
2016-09-26 15:57:18 -0400

    Edited Attack Animation

--- Animation.js
+++ Animation.js
@@ -2,26 +2,52 @@ $(document).ready(function () {
 
     var startTop = $('#attack').css("top");
     var isMoving = false;
+    /*
+                       $('#attackbox').mouseenter(function () {
+                           if (isMoving == false) {
+                               isMoving = true;
+                               $('#attack').animate({
+                                   top: '100px'
+                               }, function () {
+                                   isMoving = false;
+                               });
+                           };
+                       });
+                       $('#attackbox').mouseenter(function () {
+                           $('#defend').animate({
+                               top: startTop
+                           });
+                       });
+                       $('#defendbox').mouseenter(function () {
+                           if (isMoving == false) {
+                               isMoving = true;
+                               $('#defend').animate({
+                                   top: '100px'
+                               }, function () {
+                                   isMoving = false;
 
-    $('#attackbox').mouseenter(function () {
-        if (!isMoving) {
+                               });
+                           }
+                       });
+                       $('#defendbox').mouseenter(function () {
+                           $('#attack').animate({
+                               top: startTop
+                           });
+                       });
+                   }); */
+    $('#attackbox').hover(function () {
+        if (isMoving == false) {
             isMoving = true;
             $('#attack').animate({
                 top: '100px'
-            }, 1000, function () {
+            }, function () {
                 isMoving = false;
             });
         }
-
     });
-    $('#attackbox').mouseleave(function () {
-        if (!isMoving || isMoving) {
-            isMoving = true;
-            $('#attack').animate({
-                top: startTop
-            }, 1000, function() {
-                isMoving = false;
-            });
-        }
+    $('#defendbox').hover(function () {
+        $('#attack').animate({
+            top: startTop
+        });
     });
 });
