1e7f9a7984db88d90ce8334a79bc3112ca70c297
coo1isj <coo1piperni@gmail.com>
2016-10-04 15:24:45 -0400

    Edited attack animation

--- Animation.js
+++ Animation.js
@@ -1,10 +1,17 @@
-function myFunction() {
-    document.getElementById("attbtn1").style.color = "red";
-}
-
 $(document).ready(function () {
     var startTop = $('#attack').css("top");
     var isMoving = false;
-
-
+    var playVisible = document.getElementById("attbtn2").style.visibility = "hidden";
 });
+
+function attMove() {
+    $('#attack').animate({
+        top: 100
+    }, 1000);
+    $('#attbtn1').animate({
+        top: 550
+    }, 1000, function () {
+        document.getElementById("attbtn1").style.visibility = "hidden";
+        document.getElementById("attbtn2").style.visibility = "visible";
+    });
+}
