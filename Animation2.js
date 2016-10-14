95fee30010266b152dbf019e19c5f760a99578ef
coo1isj <coo1piperni@gmail.com>
2016-10-14 15:46:52 -0400

    Added usa flag

---
+++ Animation2.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    document.getElementById("usa").style.visibility = "hidden";
+});
+
+function usaClick() {
+    document.getElementById("usa").style.visibility = "visible";
+}
