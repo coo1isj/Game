70e1a0f9c2c8a79d5a01fe8292aeb2071ea94a06
coo1isj <coo1piperni@gmail.com>
2016-10-18 15:50:06 -0400

    Added animation on army.html, created Tutorial_Map

--- Animation2.js
+++ Animation2.js
@@ -1,7 +1,11 @@
 $(document).ready(function () {
     document.getElementById("usa").style.visibility = "hidden";
+    document.getElementById("togame").style.visibility = "hidden";
+    document.getElementById("defusa").style.visibility = "hidden";
 });
 
 function usaClick() {
     document.getElementById("usa").style.visibility = "visible";
+    document.getElementById("togame").style.visibility = "visible";
+    document.getElementById("defusa").style.visibility = "visible";
 }
